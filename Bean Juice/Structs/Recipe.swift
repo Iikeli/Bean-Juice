d504348b2251f09a4513d6253d0fc00fff5bd837
iiroalhonen <iiro.alhonen@kpsbremen.de>
2021-08-25 13:56:25 +0200

    New recipe struct for downloaded recipes.

--- Bean Juice/Structs/Recipe.swift
+++ Bean Juice/Structs/Recipe.swift
@@ -8,6 +8,10 @@
 
 import Foundation
 
+/**
+ The user created recipe
+ - Remark: to be depricated.
+ */
 struct Recipe: Codable {
     let name: String
     let brewMethod: MethodName
@@ -16,4 +20,30 @@ struct Recipe: Codable {
     let waterAmount: Int
     let brewTime: Int
     let additionalInformation: String?
+}
+
+/**
+ Downloaded recipe.
+ */
+struct DownloadedRecipe: Codable {
+    /// Recipe name.
+    let name: String
+    /// URL for an image to be shown.
+    let imageURL: String?
+    /// Brew method the repice is created for.
+    let method: String?
+    /// Ground size used.
+    let groundSize: String?
+    /// Coffee bean used.
+    let bean: String?
+    /// Ratio used.
+    let ratio: Int?
+    /// Water amount used.
+    let water: Int?
+    /// Brew time of the recipe, used in the timer.
+    let brewTime: Int?
+    /// Additional information to be shown for the user.
+    let additionalInformation: String?
+    /// Is the user able to edit the recipes values.
+    let editable: Bool
 }
